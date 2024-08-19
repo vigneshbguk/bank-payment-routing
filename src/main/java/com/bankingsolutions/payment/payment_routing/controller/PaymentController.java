f9f45a904bfbf5ebac0fc5b7f588bbecc09d0dc3
vigneshbguk <vigneshbguk@gmail.com>
2024-08-19 16:48:59 +0100

    Implemented Payment Service with Dijkstra's algorithm

--- src/main/java/com/bankingsolutions/payment/payment_routing/controller/PaymentController.java
+++ src/main/java/com/bankingsolutions/payment/payment_routing/controller/PaymentController.java
@@ -1,6 +1,20 @@
 package com.bankingsolutions.payment.payment_routing.controller;
 
+import com.bankingsolutions.payment.payment_routing.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/payments")
 public class PaymentController {
 
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/route")
+    public String getPaymentRoute(
+            @RequestParam String originBranch,
+            @RequestParam String destinationBranch) {
+        return paymentService.processPayment(originBranch, destinationBranch);
+    }
 }
