d6b34274880555b33e8888c64a1aba1ded1758fb
vigneshbguk <vigneshbguk@gmail.com>
2024-08-19 12:32:36 +0100

    Maven Project

---
+++ src/main/java/com/bankingsolutions/payment/payment_routing/PaymentRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.bankingsolutions.payment.payment_routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRoutingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingsolutions/payment/payment_routing/PaymentRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingsolutions.payment.payment_routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
